c8de310f8d181cafcaf3fc1aad68c911c012dee8
Mirsad <mirsad.hasic8@gmail.com>
2019-03-17 13:47:26 +0100

    new project

---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Parallaxing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour
+{
+
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing = 1f;
+
+    private Transform cam;
+    private Vector3 previosCamPosition;
+
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    void Start()
+    {
+        previosCamPosition = cam.position;
+
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+        
+            float parallax = (previosCamPosition.x - cam.position.x) * parallaxScales[i];
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            //Creating new vecter 3 on background target position x, whish is defined
+            //by the previous position + parallax
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            //The new background position is a lerp (smooth transition) between the intial position
+            //and the target position, all regulated or timed by 'smoothing'
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+
+        }
+
+        previosCamPosition = cam.position;
+    }
+}
---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float pWalkSpeed = 20f;
+    Rigidbody2D rb2d;
+    public float scaleX;
+    public float scaleY;
+    public float scaleZ;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();   
+    }
+
+
+     void Update()
+    {
+
+        //Turning the player sprite around it's pivot when it moves in x and -x
+        if(Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-scaleX, scaleY, scaleZ);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Grabbing horizontal and vertical input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Easing down the rb's velocity
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.x *= 0.75f;                //dividing it's velocity by a certain ammount
+        easeVelocity.y = rb2d.velocity.y;       //keeping the y velocity same not to ruin it
+        easeVelocity.z = 0.0f;                  //we don't use z axis in 2d games
+
+        rb2d.velocity = easeVelocity;
+
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement*pWalkSpeed);
+        
+        
+    }
+}
