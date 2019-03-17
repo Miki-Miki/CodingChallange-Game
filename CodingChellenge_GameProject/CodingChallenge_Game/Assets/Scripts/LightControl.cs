f05691eff0661dfa3ae59f9312a7f11759951828
Mirsad <mirsad.hasic8@gmail.com>
2019-03-17 19:30:55 +0100

    added monitor on/off interaction

--- CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/LightControl.cs
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/LightControl.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class LightControl : MonoBehaviour
 {
+    private bool isInteractable;
+    public GameObject mainObject;
+    public GameObject light;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        light.GetComponent<Light>().enabled = false;
+        isInteractable = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInteractable == true && Input.GetButtonDown("Use"))
+        {
+            Debug.Log("E is pressed");
+            if (light.GetComponent<Light>().enabled == false)
+            {
+                light.GetComponent<Light>().enabled = true;
+            }
+            else if (light.GetComponent<Light>().enabled == true)
+            {
+                light.GetComponent<Light>().enabled = false;
+            }
+
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision with Player");
+            isInteractable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player exited the collider");
+            isInteractable = false;
+        }
+    }
+
+
 }
