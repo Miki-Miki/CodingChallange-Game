8dcf870862a3f57e1f66bccf7f19ec80619c3efb
Mirsad <mirsad.hasic8@gmail.com>
2019-03-19 22:39:17 +0100

    added desktop description

---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/DescriptionPopUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DescriptionPopUp : MonoBehaviour
+{
+    public Animator animator;
+    public string boolTrigger;
+    private bool isTrigger;
+    public GameObject computerLight;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTrigger && Input.GetButtonDown("Description")
+            && computerLight.GetComponent<Light>().enabled == false)
+        {
+            animator.SetBool(boolTrigger, isTrigger);
+        } 
+        if (isTrigger == false)
+        {
+            animator.SetBool(boolTrigger, false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            isTrigger = false;
+        }
+    }
+}
