c31dfae9845a524aebb898bcdfd91260804c9118
Mirsad <mirsad.hasic8@gmail.com>
2019-03-31 20:31:03 +0200

    started portal implementation

---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/InstantiatePortal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatePortal : MonoBehaviour
+{
+    public Transform portalSpawnPoint;
+    public GameObject portal;
+    private bool isF_pressed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("InstantiatePortal") && isF_pressed == false)
+        {
+            isF_pressed = true;
+            Instantiate_Portal();
+        }
+    }
+
+    void Instantiate_Portal()
+    {
+        Instantiate(portal, portalSpawnPoint.position, portalSpawnPoint.rotation);
+    }
+}
