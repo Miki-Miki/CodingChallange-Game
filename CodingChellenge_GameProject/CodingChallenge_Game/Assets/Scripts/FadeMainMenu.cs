ef8cc27f611a47518f61292cb57993992d9b5c6c
Mirsad <mirsad.hasic8@gmail.com>
2019-03-31 18:07:32 +0200

    added main menu fade -> transition to main scene

--- CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/FadeMainMenu.cs
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/FadeMainMenu.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FadeMainMenu : MonoBehaviour
 {
     public Animator animator;
+    public Button playButton;
 
+    void OnEnable() 
+    {
+        playButton.onClick.AddListener(runFadeAnimation);
+    }
+    
     public void LoadScene()
     {
         SceneManager.LoadScene("Scene1");
     }
-    
-    void Update() 
+
+    public void runFadeAnimation() 
     {
-        
+        animator.SetBool("playPressed", true);
     }
 }
