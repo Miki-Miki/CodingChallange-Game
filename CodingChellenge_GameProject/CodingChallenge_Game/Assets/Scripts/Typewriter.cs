91ea233714a38bde08d3865be4cca452dc1996a6
WeH1D <haticv92@gmail.com>
2019-04-29 19:16:54 +0200

    commint

---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/Typewriter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typewriter : MonoBehaviour
+{
+    Text txt;
+    string story;
+    public bool PlayOnAwake = true;
+    public float Delay;
+
+    void Awake()
+    {
+        txt = GetComponent<Text>();
+        if (PlayOnAwake)
+            ChangeText(txt.text, Delay);
+    }
+
+    //Update text and start typewriter effect
+    public void ChangeText(string _text, float _delay = 0f)
+    {
+        StopCoroutine(PlayText()); //stop Coroutime if exist
+        story = _text;
+        txt.text = ""; //clean text
+        Invoke("Start_PlayText", _delay); //Invoke effect
+    }
+
+    void Start_PlayText()
+    {
+        StartCoroutine(PlayText());
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach (char c in story)
+        {
+            txt.text += c;
+            yield return new WaitForSeconds(0.125f);
+        }
+    }
+}
