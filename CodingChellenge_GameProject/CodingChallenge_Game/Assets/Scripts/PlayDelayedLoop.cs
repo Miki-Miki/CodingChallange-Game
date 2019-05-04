dbc30c4f46338047680d3547e727a7ae52583c0a
Mirsad <mirsad.hasic8@gmail.com>
2019-05-04 20:24:09 +0200

    working on audio

---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/Scripts/PlayDelayedLoop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayDelayedLoop : MonoBehaviour
+{
+    public AudioClip first;
+    public AudioClip looped;
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = first;
+        audio.Play();
+        audio.clip = looped;
+        audio.PlayDelayed(first.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
