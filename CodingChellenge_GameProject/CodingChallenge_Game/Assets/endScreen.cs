3a82a2b24661baa738dbc0d9290bec3054149b50
WeH1D <haticv92@gmail.com>
2019-05-14 20:00:08 +0200

    end screen and vines

---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/endScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endScreen : MonoBehaviour
+{
+    public GameObject endScreenCanvas;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            endScreenCanvas.SetActive(true);
+        }
+    }
+}
