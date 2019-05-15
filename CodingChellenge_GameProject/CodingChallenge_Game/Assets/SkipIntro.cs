e108b0738de85a3463f0f843af345728777443d2
WeH1D <haticv92@gmail.com>
2019-05-15 14:58:05 +0200

    version control

---
+++ CodingChellenge_GameProject/CodingChallenge_Game/Assets/SkipIntro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipIntro : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+}
